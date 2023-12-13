ccd6ee55a45bae14e29010310c08c29543739260
owwwl666 <zaharsalam228@gmail.com>
2023-12-14 05:52:41 +1000

    setup DialogFlow.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from environs import Env
+from google.cloud import dialogflow
 
 env = Env()
 env.read_env()
@@ -7,14 +8,37 @@ env.read_env()
 bot = telebot.TeleBot(env.str("TELEGRAM_BOT_TOKEN"))
 
 
-@bot.message_handler(commands=['start'])
+def get_reply_to_message(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
+
+
+@bot.message_handler(commands=["start"])
 def start_work(message):
     bot.send_message(message.chat.id, "Здравствуйте!")
 
 
 @bot.message_handler(content_types=["text"])
 def replie_to_message(message):
-    bot.reply_to(message, message.text)
+    response = get_reply_to_message(
+        project_id=env.str("GOOGLE_PROJECT_ID"),
+        session_id=message.chat.id,
+        text=message.text,
+        language_code="en-US"
+    )
+
+    bot.send_message(message.chat.id, response)
 
 
-bot.infinity_polling(none_stop=True)
+if __name__ == "__main__":
+    bot.infinity_polling(none_stop=True)
